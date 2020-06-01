92b7b7f58d2d8e201ef66d9f4d630cd4c0711d3b
Ethan Hong <20248019+ethanhong@users.noreply.github.com>
2020-06-01 14:42:19 -0700

    use run() instead of Popen.communicate() and add timeout flow

--- clash-of-empires-robot-v2/adb.py
+++ clash-of-empires-robot-v2/adb.py
@@ -19,19 +19,23 @@ SUCCESS = 0
 ERROR = -1
 
 
-# def adb_shell():
-#     return 'adb -s localhost:{} shell '.format(current_port)
-
-
-def adb_command(cmd):
-    cmd = cmd.split()
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    out = p.communicate()[0]
-    return out.decode("utf-8")
+def run(cmd, timeout=30, retry=3):
+    for _ in range(retry):
+        try:
+            process = subprocess.run(cmd, stdout=subprocess.PIPE, text=True, timeout=timeout)
+            out = process.stdout
+            return out
+        except subprocess.TimeoutExpired:
+            print('[Adb command timeout]:', cmd)
+            subprocess.run('adb disconnect localhost:{}'.format(current_port))
+            subprocess.run('adb connect localhost:{}'.format(current_port))
+            continue
+    else:
+        raise subprocess.TimeoutExpired
 
 
 def connect(port):
-    out = adb_command('adb connect localhost:{}'.format(port))
+    out = run('adb connect localhost:{}'.format(port))
     if 'connected to localhost:{}'.format(port) in out:
         return SUCCESS
     else:
@@ -39,30 +43,30 @@ def connect(port):
 
 
 def disconnect(port):
-    adb_command('adb disconnect localhost:{}'.format(port))
+    run('adb disconnect localhost:{}'.format(port))
 
 
 def kill_server():
-    adb_command('adb kill-server')
+    run('adb kill-server')
 
 
 def launch_coe():
-    adb_command('adb -s localhost:{} shell am start -n com.leme.coe/com.leme.coe.MainActivity'.format(current_port))
+    run('adb -s localhost:{} shell am start -n com.leme.coe/com.leme.coe.MainActivity'.format(current_port))
 
 
 def force_stop_coe():
-    adb_command('adb -s localhost:{} shell am force-stop com.leme.coe'.format(current_port))
+    run('adb -s localhost:{} shell am force-stop com.leme.coe'.format(current_port))
 
 
 def screenshot():
-    adb_command('adb -s localhost:{} shell screencap -p /sdcard/screen.png'.format(current_port))
-    adb_command('adb -s localhost:{} pull /sdcard/screen.png'.format(current_port))
-    adb_command('adb -s localhost:{} shell rm /sdcard/screen.png'.format(current_port))
+    run('adb -s localhost:{} shell screencap -p /sdcard/screen.png'.format(current_port))
+    run('adb -s localhost:{} pull /sdcard/screen.png'.format(current_port))
+    run('adb -s localhost:{} shell rm /sdcard/screen.png'.format(current_port))
     return PIL.Image.open('screen.png').convert('RGB')
 
 
 def tap(coord):
-    adb_command('adb -s localhost:{} shell input tap {} {}'.format(current_port, coord[0], coord[1]))
+    run('adb -s localhost:{} shell input tap {} {}'.format(current_port, coord[0], coord[1]))
     time.sleep(DELAY_BETWEEN_SCREEN_EVENT)
 
 
@@ -70,28 +74,28 @@ def swipe(moves, duration=3000):
     tl, tr, ll, lr = (170, 380), (370, 380), (170, 580), (370, 580)
     for move in moves:
         if move == 'up':
-            adb_command('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
+            run('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
                 current_port, ll[0], ll[1], tl[0], tl[1], duration))
         elif move == 'down':
-            adb_command('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
+            run('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
                 current_port, tl[0], tl[1], ll[0], ll[1], duration))
         elif move == 'left':
-            adb_command('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
+            run('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
                 current_port, lr[0], lr[1], ll[0], ll[1], duration))
         elif move == 'right':
-            adb_command('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
+            run('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
                 current_port, ll[0], ll[1], lr[0], lr[1], duration))
         elif move == 'top_right':
-            adb_command('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
+            run('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
                 current_port, ll[0], ll[1], tr[0], tr[1], duration))
         elif move == 'top_left':
-            adb_command('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
+            run('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
                 current_port, lr[0], lr[1], tl[0], tl[1], duration))
         elif move == 'lower_right':
-            adb_command('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
+            run('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
                 current_port, tl[0], tl[1], lr[0], lr[1], duration))
         elif move == 'lower_left':
-            adb_command('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
+            run('adb -s localhost:{} shell input swipe {} {} {} {} {}'.format(
                 current_port, tr[0], tr[1], ll[0], ll[1], duration))
         time.sleep(DELAY_BETWEEN_SCREEN_EVENT)
 
